bbff5072bf43b7eab15ccaa81fc6b8e5b39ab4e0
Priscila Elias <priscilaek@gmail.com>
2023-07-15 10:56:15 -0500

    objetos1

--- js/index.js
+++ js/index.js
@@ -1,14 +1,39 @@
-console.log('hola mundo')
-console.log('hola mundo')
-console.log('hola mundo')
-console.log('hola mundo')
+console.log("hola mundo")
 
 const data = {
-    name: 'Juan',
-    "real age": 25, 
-    country:'MX'
+  name: "Juan",
+  "real age": 25,
+  country: "MX",
+  "favorite food": "tacos",
+  "favorite food categories": {
+    mexican: "tacos",
+    italian: "pasta"
+  },
+  screens: ["lg", "hp"]
 }
 
-console.log(data.name)
+// OBJETOS
 
-console.log(data["real age"]) //bracket notation
+// 1. DOT NOTATION
+console.log(data.name) // "Juan"
+
+// 2. BRACKET NOTATION
+console.log(data["real age"])
+console.log(data["favorite food"])
+
+// MIX
+console.log(
+  "acceder por bracket notation",
+  data["favorite food categories"].mexican
+)
+console.log(
+  "acceder por bracket notation",
+  data["favorite food categories"].italian
+)
+
+console.log(data["favorite food categories"]["mexican"])
+console.log(data["favorite food categories"]["italian"])
+
+// ARREGLOS
+console.log(data.screens[0])
+console.log(data.screens[1])
