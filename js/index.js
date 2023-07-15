daa7d43001d00e45064c1c9c20bfe6a57af465d3
Priscila Elias <priscilaek@gmail.com>
2023-07-15 10:46:33 -0500

    ignoring3

---
+++ js/index.js
@@ -0,0 +1,14 @@
+console.log('hola mundo')
+console.log('hola mundo')
+console.log('hola mundo')
+console.log('hola mundo')
+
+const data = {
+    name: 'Juan',
+    "real age": 25, 
+    country:'MX'
+}
+
+console.log(data.name)
+
+console.log(data["real age"]) //bracket notation
